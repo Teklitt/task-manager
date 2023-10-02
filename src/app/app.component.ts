03c9936a508388630999415c721b8204c8f16843
Teklitt <mebudenifemi@gmail.com>
2023-10-02 08:34:56 -0400

    adjustment to front end calling api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,24 +6,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'task-manager';
-  readonly APIUrl = "http://localhost:3000/api/toDoApp/";
-
-  constructor(private http:HttpClient){
-  }
-  //an array to store retrieved data
-  notes:any=[];
-
-  //sends GET request to the api url & subscribes to response. response is stored in notes array
-  refreshNotes(){
-    this.http.get<any[]>(this.APIUrl+'GetToDo').subscribe(data=>{
-      this.notes=data;
-    })
-  }
-
-    //fetches and displays initial data when component is initialized
-  ngOnInit(){
-    this.refreshNotes();
-  }
-}
+export class AppComponent {}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
-import { AppComponent } from '../app.component';
-
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
+  title = 'task-manager';
+  readonly APIUrl = 'http://localhost:3000/api/toDoApp/';
 
+  constructor(private http: HttpClient) {}
+  //an array to store retrieved data
+  notes: any = [];
+
+  //sends GET request to the api url & subscribes to response. response is stored in notes array
+  refreshNotes() {
+    this.http.get<any[]>(this.APIUrl + 'GetToDo').subscribe((data) => {
+      this.notes = data;
+    });
+  }
+
+  //fetches and displays initial data when component is initialized
+  ngOnInit() {
+    this.refreshNotes();
+  }
 }
